561176aab11d3d1d58661c2618ab10707dcbeb47
toniferrer <toniferrerhol@gmail.com>
2026-03-25 17:58:12 +0100

    Se ha creado el archivo dades-mock.ts

---
+++ src/app/mocks/dades-mock.ts
@@ -0,0 +1,74 @@
+import { Links } from "../models/links.model";
+
+export const LINKS_MOCK: Links[] = [
+    {
+        id: 0,
+        titulo: 'Elecciones generales de Dinamarca de 2026',
+        enlace: 'https://es.wikipedia.org/wiki/Elecciones_generales_de_Dinamarca_de_2026',
+        visitas: 0,
+        visible: true
+    },
+    {
+        id: 1,
+        titulo: 'Echinaster sepositus',
+        enlace: 'https://es.wikipedia.org/wiki/Echinaster_sepositus',
+        visitas: 0,
+        visible: true
+    },
+    {
+        id: 2,
+        titulo: 'Barcelona-Estación de Francia',
+        enlace: 'https://es.wikipedia.org/wiki/Barcelona-Estaci%C3%B3n_de_Francia',
+        visitas: 0,
+        visible: true
+    },
+    {
+        id: 3,
+        titulo: 'Parque Güell',
+        enlace: 'https://es.wikipedia.org/wiki/Parque_G%C3%BCell',
+        visitas: 0,
+        visible: true
+    },
+    {
+        id: 4,
+        titulo: 'Volta a Cataluña 2026',
+        enlace: 'https://es.wikipedia.org/wiki/Volta_a_Catalu%C3%B1a_2026',
+        visitas: 0,
+        visible: true
+    },
+    {
+        id: 5,
+        titulo: 'Día Mundial de la Tuberculosis',
+        enlace: 'https://es.wikipedia.org/wiki/D%C3%ADa_Mundial_de_la_Tuberculosis',
+        visitas: 0,
+        visible: true
+    },
+    {
+        id: 6,
+        titulo: 'Organización Mundial de la Salud',
+        enlace: 'https://es.wikipedia.org/wiki/Organizaci%C3%B3n_Mundial_de_la_Salud',
+        visitas: 0,
+        visible: true
+    },
+    {
+        id: 7,
+        titulo: 'La peor persona del mundo',
+        enlace: 'https://es.wikipedia.org/wiki/La_peor_persona_del_mundo',
+        visitas: 0,
+        visible: true
+    },
+    {
+        id: 8,
+        titulo: 'Premios del Cine Europeo',
+        enlace: 'https://es.wikipedia.org/wiki/Premios_del_Cine_Europeo',
+        visitas: 0,
+        visible: true
+    },
+    {
+        id: 9,
+        titulo: 'Centro de Conferencias del Mediterráneo',
+        enlace: 'https://es.wikipedia.org/wiki/Centro_de_Conferencias_del_Mediterr%C3%A1neo',
+        visitas: 0,
+        visible: true
+    },
+]
